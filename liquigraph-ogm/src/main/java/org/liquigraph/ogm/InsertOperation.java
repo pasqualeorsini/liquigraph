b33d539253d5191b9c9dd78adf10fbe128a3f5ea
Pascal Orsini <pasquale.orsini@gmail.com>
2017-06-27 05:08:08 +0200

    First draft of the insert operation

--- liquigraph-ogm/src/main/java/org/liquigraph/ogm/InsertOperation.java
+++ liquigraph-ogm/src/main/java/org/liquigraph/ogm/InsertOperation.java
@@ -1,7 +1,20 @@
 package org.liquigraph.ogm;
 
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.RelationshipEntity;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
 public class InsertOperation {
-    public InsertOperation(String entityName, OgmProperty property) {
+    private final String entityName;
+    private final List<OgmProperty> properties;
+
+    public InsertOperation(String entityName, List<OgmProperty> properties) {
+
+        this.entityName = entityName;
+        this.properties = properties;
     }
 
     public Object resolveEntity() {
@@ -9,8 +22,39 @@ public class InsertOperation {
         // edge cases (TODO unit tests) :
         //  - class exists
         //  - class is an OGM entity (@NodeEntity / @RelationEntity)
-        //  - property exists and value type is compatible
-        //  - do not assign @GraphId property !!
+        //  - properties exists and value type is compatible
+        //  - do not assign @GraphId properties !!
+        Class<?> entityClass = null;
+        try {
+            entityClass = Class.forName(this.entityName);
+            NodeEntity[] nodeAnnotation = entityClass.getDeclaredAnnotationsByType(NodeEntity.class);
+            RelationshipEntity[] relationshipAnnotation = entityClass.getDeclaredAnnotationsByType(RelationshipEntity.class);
+            if(relationshipAnnotation.length>0 || nodeAnnotation.length > 0){
+                Object instance = entityClass.newInstance();
+                for (OgmProperty property : this.properties) {
+                    setValue(entityClass, instance, property.getName(), property.getValue());
+                }
+            }
+        } catch (ClassNotFoundException | IllegalAccessException | NoSuchFieldException | InstantiationException e) {
+            return null;
+        }
+
         return null;
     }
+
+    private  static void setValue(Class clazz, Object entity, String name, Object value) throws NoSuchFieldException, IllegalAccessException {
+        Field declaredField = clazz.getDeclaredField(name);
+        declaredField.setAccessible(true);
+        GraphId[] graphIdAnnotation = declaredField.getAnnotationsByType(GraphId.class);
+        if(graphIdAnnotation.length==0) {
+            if (declaredField.getType().equals(Long.class)) {
+                declaredField.set(entity, Long.parseLong(value));
+            } else {
+                declaredField.set(entity, value);
+            }
+            declaredField.setAccessible(false);
+        }
+
+    }
+
 }
--- liquigraph-ogm/src/main/java/org/liquigraph/ogm/OgmProperty.java
+++ liquigraph-ogm/src/main/java/org/liquigraph/ogm/OgmProperty.java
@@ -1,6 +1,20 @@
 package org.liquigraph.ogm;
 
 public class OgmProperty {
+    private final String name;
+    private final Object value;
+
     public OgmProperty(String name, Object value) {
+
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Object getValue() {
+        return value;
     }
 }
